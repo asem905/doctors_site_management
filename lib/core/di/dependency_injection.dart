fc067e10eb01db1f3db89a807b9e79a514d5280b
Assem Samy <asemosamyy@gmail.com>
2025-09-20 21:17:43 +0300

    added all signup features as login exactly

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -3,6 +3,7 @@ import 'package:flutter_complete_proj/core/networking/api_service.dart';
 import 'package:flutter_complete_proj/core/networking/dio_factory.dart';
 import 'package:flutter_complete_proj/features/login/data/repos/login_Repo.dart';
 import 'package:flutter_complete_proj/features/login/logic/cubit/login_cubit.dart';
+import 'package:flutter_complete_proj/features/signup/logic/cubit/sign_up_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt=GetIt.instance;
@@ -12,4 +13,5 @@ Future<void> setupGetIt()async{
   getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
   getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+  getIt.registerFactory<SignupCubit>(()=> SignupCubit(getIt()));
 }
